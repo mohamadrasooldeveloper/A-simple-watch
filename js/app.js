cec36e1459ef89f93d0378f44ef62de6c5f7bcc4
mohamadrasool <msj.developer3133@gmail.com>
2024-11-05 23:10:18 +0330

    watch

---
+++ js/app.js
@@ -0,0 +1,30 @@
+let $ = document
+
+const clock = $.querySelector('.clock')
+const hourElem = $.getElementById('hours')
+const minuteElem = $.getElementById('minutes')
+const secondElem = $.getElementById('seconds')
+
+setInterval(function () {
+    let myDate = new Date()
+
+    let nowHour = myDate.getHours()
+    let nowMinute = myDate.getMinutes()
+    let nowSecond = myDate.getSeconds()
+
+    if (nowHour < 10) {
+        nowHour = '0' + nowHour
+    }
+    if (nowMinute < 10) {
+        nowMinute = '0' + nowMinute
+    }
+    if (nowSecond < 10) {
+        nowSecond = '0' + nowSecond
+    }
+
+    hourElem.innerHTML = nowHour;
+    minuteElem.innerHTML = nowMinute;
+    secondElem.innerHTML = nowSecond;
+
+    console.log(myDate);
+}, 1000);
